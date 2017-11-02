61f1ecbace869ccd1482e2a189ead40137048c90
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-11-02 17:48:13 -0200

    M1#A09 - getDefaultProps

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -10,9 +10,7 @@ const App = React.createClass({
   render: function () {
     return (
       <div className='container'>
-        <Title name='Fernando Daciuk' />
-        <label htmlFor='input' data-label='Label'>Input</label>
-        <input type='text' id='input' aria-hidden />
+        <Title lastname='Daciuk' />
       </div>
     )
   }
