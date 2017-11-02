9e75ebc1e68290f4f73d8ee0dccba41252c6e3eb
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-11-02 18:09:02 -0200

    M1#A11 - Renderizando componentes com funções puras

--- react-webpack/src/title.js
+++ react-webpack/src/title.js
@@ -1,30 +1,16 @@
+/* eslint no-unused-vars: 0 */
+
 'use strict'
 
 import React from 'react'
 
-const Title = React.createClass({
-  getDefaultProps: function () {
-    return {
-      name: 'Desconhecido',
-      lastname: {
-        first: 'Sem',
-        last: 'sobrenome'
-      }
-    }
-  },
+const Title = ({ name, lastname }) => (
+  <h1>Olá {`${name} ${lastname}`}</h1>
+)
 
-  render: function () {
-    return (
-      <h1>
-        Olá
-        {
-          this.props.name + ' ' +
-          this.props.lastname.first +
-          this.props.lastname.last
-        }!
-      </h1>
-    )
-  }
-})
+Title.defaultProps = {
+  name: 'Desconhecido',
+  lastname: 'Sem sobrenome'
+}
 
 export default Title
