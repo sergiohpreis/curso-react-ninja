18ac700619c4932414e163f2c496dbaf6b0e2886
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-11-02 12:38:21 -0200

    Colocando o hot loader para funcionar

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -2,10 +2,10 @@
 
 import React from 'react'
 
-var Title = React.createClass({
+const App = React.createClass({
   render: function () {
-    return <h1>Título</h1>
+    return <div>Aplicação</div>
   }
 })
 
-export default Title
+export default App
