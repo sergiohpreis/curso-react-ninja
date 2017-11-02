6db6e6552e4f59b3468946832b4120c148b1ad4e
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-11-02 12:56:27 -0200

    M1#A07 - Passando props

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -1,10 +1,18 @@
+/* eslint no-unused-vars: 0 */
+
 'use strict'
 
 import React from 'react'
 
+import Title from './title'
+
 const App = React.createClass({
   render: function () {
-    return <div>Aplicação</div>
+    return (
+      <div>
+        <Title name='Fernando Daciuk' />
+      </div>
+    )
   }
 })
 
