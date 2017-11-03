43ff0f42355d50e862f1c13e22e7850ce33a3a3c
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-11-03 08:26:40 -0200

    M1#A12 - Renderizando componentes com classes do ES6/2015

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -2,18 +2,18 @@
 
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
 
 import Title from './title'
 
-const App = React.createClass({
-  render: function () {
+class App extends Component {
+  render () {
     return (
       <div className='container'>
         <Title name='Fernando' />
       </div>
     )
   }
-})
+}
 
 export default App
