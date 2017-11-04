c2d630a012b6cf7a14b5b1135ee4c3041ecbd3d8
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-11-04 10:47:42 -0200

    M1#A20 - Stateful vs Stateless

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -3,23 +3,30 @@
 'use strict'
 
 import React, { Component } from 'react'
-import LikeButton from './like-button'
-import SearchButton from './search-button'
+import Button from './button'
+import Square from './square'
 
 class App extends Component {
   constructor () {
     super()
     this.state = {
-      text: 'Fernando'
+      color: 'green'
     }
   }
 
   render () {
     return (
-      <div className='container' onClick={() => this.setState({
-        text: 'Outro texto'
-      })}>
-        {this.state.text}
+      <div>
+        <Square color={this.state.color} />
+
+        {['red', 'green', 'blue'].map(color => (
+          <Button
+            key={color}
+            handleClick={() => this.setState({ color })}
+          >
+            {color}
+          </Button>
+        ))}
       </div>
     )
   }
