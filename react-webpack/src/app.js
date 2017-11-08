d7c4657bdb28e30d3582d260159f100dab12c1e6
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-11-08 01:28:08 -0200

    M1#A28 - Introdução à formulários no React

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -3,15 +3,25 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Button from './button'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      value: 'Valor inicial'
+    }
+  }
+
   render () {
     return (
       <div>
-        <Button>
-          Clique em mim
-        </Button>
+        <form>
+          <input type='text' value={this.state.value} onChange={(e) => {
+            this.setState({
+              value: e.target.value
+            })
+          }} />
+        </form>
       </div>
     )
   }
