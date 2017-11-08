2348a9707448090b31236371c28eb4c808826109
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-11-08 09:09:21 -0200

    M1#A29 - Formulários (checkbox e radio)

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      value: 'Valor inicial'
+      value: 'Valor inicial',
+      checked: false
     }
   }
 
@@ -21,6 +22,20 @@ class App extends Component {
               value: e.target.value
             })
           }} />
+
+          <label>
+            <input
+              type='checkbox'
+              value='my-checkbox'
+              checked={this.state.checked}
+              onChange={(e) => {
+                this.setState({ checked: !this.state.checked })
+              }} />
+            Checkbox
+          </label>
+
+          <input type='radio' name='rd' value='1' defaultChecked /> Radio 1
+          <input type='radio' name='rd' value='2' /> Radio 2
         </form>
       </div>
     )
