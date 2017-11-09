6a72e287cd548d43f71e65b379cee1b0a1921e17
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-11-09 08:42:39 -0200

    M1#A35 - GitHub App - Criando a marcação da aplicação

--- github-app/src/app.js
+++ github-app/src/app.js
@@ -7,8 +7,43 @@ import React, { Component } from 'react'
 class App extends Component {
   render () {
     return (
-      <div>
-        My App
+      <div className='app'>
+        <div className='search'>
+          <input type='search'
+            placeholder='Digite o nome do usuário no Github' />
+        </div>
+
+        <div className='user-info'>
+          <img src='https://avatars2.githubusercontent.com/u/487669?v=4' />
+          <h1>
+            <a href='https://github.com/fdaciuk'>Fernando Daciuk</a>
+          </h1>
+
+          <ul className='repos-info'>
+            <li>Repositórios: 122</li>
+            <li>Seguidores: 10</li>
+            <li>Seguindo: 10</li>
+          </ul>
+
+          <div className='actions'>
+            <button>Ver repositórios</button>
+            <button>Ver favoritos</button>
+          </div>
+
+          <div className='repos'>
+            <h2>Repositórios:</h2>
+            <ul>
+              <li><a href='#'>Nome do repositório</a></li>
+            </ul>
+          </div>
+
+          <div className='starred'>
+            <h2>Favoritos:</h2>
+            <ul>
+              <li><a href='#'>Nome do repositório</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
