1492ad61e72bf65743ccfd0c5451391b2f932a22
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-11-09 08:24:45 -0200

    GitHub App - Commit Inicial

---
+++ github-app/src/app.js
@@ -0,0 +1,17 @@
+/* eslint no-unused-vars: 0 */
+
+'use strict'
+
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        My App
+      </div>
+    )
+  }
+}
+
+export default App
