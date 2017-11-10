f68cd0b929f1964f9f5c0745d7d70b0a6666323e
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-11-10 08:30:54 -0200

    M1#A37 - GitHub app - Criando o Container Component da aplicação

--- github-app/src/app.js
+++ github-app/src/app.js
@@ -4,38 +4,16 @@
 
 import React, { Component } from 'react'
 
-import Search from './components/search'
-import UserInfo from './components/user-info'
-import Actions from './components/actions'
-import Repos from './components/repos'
+import AppContent from './components/app-content'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {}
+  }
+
   render () {
-    return (
-      <div className='app'>
-        <Search />
-        <UserInfo />
-        <Actions />
-
-        <Repos
-          className='repos'
-          title='Repositórios:'
-          repos={[{
-            name: 'Nome do repositório',
-            link: '#'
-          }]}
-        />
-
-        <Repos
-          className='starred'
-          title='Favoritos:'
-          repos={[{
-            name: 'Nome do repositório',
-            link: '#'
-          }]}
-        />
-      </div>
-    )
+    return <AppContent />
   }
 }
 
