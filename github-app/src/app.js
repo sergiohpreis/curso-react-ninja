8120cd78abbc99302df6fe3746e99ac3583cca7e
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-11-12 11:06:26 -0200

    M1#A44 - GitHub app - buscar repositórios do usuário pesquisado

--- github-app/src/app.js
+++ github-app/src/app.js
@@ -17,13 +17,19 @@ class App extends Component {
     }
   }
 
+  getGitHubApiUrl (username, type) {
+    const internalUser = username ? `/${username}` : ''
+    const internalType = type ? `/${type}` : ''
+    return `https://api.github.com/users${internalUser}${internalType}`
+  }
+
   handleSearch (e) {
     const value = e.target.value
     const keyCode = e.which || e.keyCode
     const ENTER = 13
 
     if (keyCode === ENTER) {
-      ajax().get(`https://api.github.com/users/${value}`)
+      ajax().get(this.getGitHubApiUrl(value))
         .then(result => {
           this.setState({
             userinfo: {
@@ -33,7 +39,9 @@ class App extends Component {
               repos: result.public_repos,
               followers: result.followers,
               following: result.following
-            }
+            },
+            repos: [],
+            starred: []
           })
         })
     }
@@ -41,7 +49,8 @@ class App extends Component {
 
   getRepos (type) {
     return (e) => {
-      ajax().get(`https://api.github.com/users/fdaciuk/${type}`)
+      const { login } = this.state.userinfo
+      ajax().get(this.getGitHubApiUrl(login, type))
         .then(result => {
           this.setState({
             [type]: result.map(repo => ({
