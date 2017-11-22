4e259ac6310d17b26c930659c1326d019159175b
Sergio Henrique Pereira Reis <sergio.reis@hiplatform.com>
2017-11-22 18:44:41 -0200

    M2#A12 - TDD - map recursivo

---
+++ examples/m02/jest-in-practice/src/map/map-recursive.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const map = (arr = [], func = item => item) => {
+    if (arr.length === 0) {
+        return []
+    }
+
+    const [head, ...tail] = arr;
+    return [func(head)].concat(map(tail, func))
+}
+
+export default map
