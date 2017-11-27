95617aadbca0504c039dce9dc13a8aed09019f09
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-11-27 09:05:07 -0200

    M2#A16 - TDD - every

--- examples/m02/jest-in-practice/src/every/every.js
+++ examples/m02/jest-in-practice/src/every/every.js
@@ -1,16 +1,13 @@
 'use strict'
 
 const every = (arr, func) => {
-    let validator = true
-
     for(let i = 0; i < arr.length; i++) {
-        if (!func(arr[i], i)) {
-            validator = false
-            break
+        if (!func(arr[i])) {
+            return false
         }
     }
 
-    return validator
+    return true
 }
 
 export default every
--- examples/m02/jest-in-practice/src/every/every.test.js
+++ examples/m02/jest-in-practice/src/every/every.test.js
@@ -5,16 +5,16 @@ import every from './every'
 
 it('every should be a function', () => {
     expect(every).to.be.a('function')
-});
+})
+
+it('every([], item => item) should return true', () => {
+    expect(every([], item => item)).to.be.ok
+})
+
+it('every([0, 2, 3], item => item) should return false', () => {
+    expect(every([0, 2, 3], item => item)).to.not.be.ok
+})
 
 it('every([1, 2, 3], item => item) should return true', () => {
-    expect(every([1, 2, 3], item => item)).to.be.equal(true)
-})
-
-it('every([0, 1, 2], item => item) should return false', () => {
-    expect(every([0, 1, 2], item => item)).to.be.equal(false)
-})
-
-it('every([0, 1, 2], (item, index) => item === index should return true', () => {
-    expect(every([0, 1, 2], (item, index) => item === index)).to.be.equal(true)
+    expect(every([1, 2, 3], item => item)).to.be.ok
 })
