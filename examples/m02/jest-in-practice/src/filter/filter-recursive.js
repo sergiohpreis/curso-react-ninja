491be14f8aa75cc02553187472d4210e9e427c44
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-11-27 08:45:08 -0200

    M2#A15 - TDD - filter recursivo

--- examples/m02/jest-in-practice/src/filter/filter-recursive.js
+++ examples/m02/jest-in-practice/src/filter/filter-recursive.js
@@ -4,11 +4,10 @@ const filter = (arr, func) => {
     return (function filterInternal(arrayInternal, counter) {
         const [head, ...tail] = arrayInternal
 
-        return arrayInternal.length === 0 ? [] : 
-            func(head, counter, arr) ? [
-                head,
-                ...filterInternal(tail, counter + 1)
-            ] : [...filterInternal(tail, counter + 1)]
+        return arrayInternal.length === 0 
+            ? []
+            : (func(head, counter, arr) ? [head] : [])
+                .concat(filterInternal(tail, counter + 1))
     })(arr, 0)
 }
 
