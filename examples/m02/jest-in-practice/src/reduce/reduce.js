f6fa2272510258746d530a12e52d688675080cc7
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-11-29 09:03:36 -0200

    M2#A22 - TDD - reduce

---
+++ examples/m02/jest-in-practice/src/reduce/reduce.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const reduce = (arr, func, initialValue) => {
+    let acc = initialValue;
+    for (let i = 0; i < arr.length; i++) {
+        acc = func(acc, arr[i])
+    }
+
+    return acc
+}
+
+export default reduce
