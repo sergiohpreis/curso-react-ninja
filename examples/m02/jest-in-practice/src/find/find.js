7f9e7782653cac5c5b4b3f6b4d95dbd7c43cad67
Sergio Henrique Pereira Reis <sergio.reis@hiplatform.com>
2017-12-05 12:44:07 -0200

    M2#A27 - TDD - find

--- examples/m02/jest-in-practice/src/find/find.js
+++ examples/m02/jest-in-practice/src/find/find.js
@@ -1,18 +1,11 @@
 'use strict'
 
 const find = (arr, func) => {
-    let foundItem = undefined
-    
     for (let i = 0; i < arr.length; i++) {
-        const item = arr[i];
-        
-        if (func(item)) {
-            foundItem = item
-            break
+        if (func(arr[i], i, arr)) {
+            return arr[i]
         }
     }
-
-    return foundItem
 }
 
 export default find
--- examples/m02/jest-in-practice/src/find/find.test.js
+++ examples/m02/jest-in-practice/src/find/find.test.js
@@ -11,10 +11,14 @@ it('find([1, 2, 3], item => item === 1) should return 1', () => {
     expect(find([1, 2, 3], item => item === 1)).to.be.equal(1)
 })
 
-it('find([2, 3, 4], item => item === 3) should return 3', () => {
-    expect(find([2, 3, 4], item => item === 3)).to.be.equal(3)
+it('find([1, 2, 3], item => item === 3) should return 3', () => {
+    expect(find([1, 2, 3], item => item === 3)).to.be.equal(3)
 })
 
-it('find([2, 3, 4], item => item === 5) should return undefined', () => {
-    expect(find([2, 3, 4], item => item === 5)).to.be.equal(undefined)
+it('find([1, 2, 3], (item, index) => index === 1) should return 2', () => {
+    expect(find([1, 2, 3], (item, index) => index === 1)).to.be.equal(2)
+})
+
+it('find([1, 2, 3], (item, index, array) => array[index] === 2) should return 2', () => {
+    expect(find([1, 2, 3], (item, index, array) => array[index] === 2)).to.be.equal(2)
 })
