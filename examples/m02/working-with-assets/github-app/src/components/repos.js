acc246e8425c284e55af5f92757c3a47b7b9669d
Sergio Henrique  Pereira Reis <sergiohpreis@Sergios-MacBook-Pro.local>
2017-12-05 22:23:12 -0200

    M2#A31 - Trabalhando com assets: configuração no Webpack (1/2)

---
+++ examples/m02/working-with-assets/github-app/src/components/repos.js
@@ -0,0 +1,28 @@
+/* eslint no-unused-vars: 0 */
+
+'use strict'
+
+import React, { PropTypes } from 'react'
+
+const Repos = ({ className, title, repos }) => (
+  <div className={className}>
+    <h2>{title}</h2>
+    <ul>
+      {repos.map((repo, index) => (
+        <li key={index}><a href={repo.link}>{repo.name}</a></li>
+      ))}
+    </ul>
+  </div>
+)
+
+React.defaultProps = {
+  className: ''
+}
+
+Repos.propType = {
+  className: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  repos: PropTypes.array.isRequired
+}
+
+export default Repos
---
+++ examples/m02/working-with-assets/github-app/src/components/search.js
@@ -0,0 +1,24 @@
+/* eslint no-unused-vars: 0 */
+
+'use strict'
+
+import React, { PropTypes } from 'react'
+import ajax from '@fdaciuk/ajax'
+
+const Search = ({ isDisabled, handleSearch }) => (
+  <div className='search'>
+    <input
+      type='search'
+      placeholder='Digite o nome do usuário no Github'
+      disabled={isDisabled}
+      onKeyUp={handleSearch}
+    />
+  </div>
+)
+
+Search.propTypes = {
+  isDisabled: PropTypes.bool.isRequired,
+  handleSearch: PropTypes.func.isRequired
+}
+
+export default Search
