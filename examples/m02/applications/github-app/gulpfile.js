03198fca1574f0eb55754bfbda261f64b869ef59
Sergio Henrique  Pereira Reis <sergiohpreis@Sergios-MacBook-Pro.local>
2017-12-20 21:42:32 -0200

    Adjusting Callback

--- examples/m02/applications/github-app/gulpfile.js
+++ examples/m02/applications/github-app/gulpfile.js
@@ -5,7 +5,7 @@ const { spawn } = require('child_process')
 
 gulp.task('lint', cb => {
   const cmd = spawn('yarn', ['lint'], { stdio: 'inherit' })
-  cmd.on('close', () => cb)
+  cmd.on('close', () => cb())
 })
 
 gulp.task('default', ['lint'], () => {
