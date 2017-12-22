23da8876e3aefaf9c5b59174a3e49c180f43f28f
Sergio Henrique  Pereira Reis <sergiohpreis@Sergios-MacBook-Pro.local>
2017-12-22 08:28:44 -0200

    M2#A53 - Paginação - implementação - parte 2

--- examples/m02/applications/github-app/src/utils/pagination/index.js
+++ examples/m02/applications/github-app/src/utils/pagination/index.js
@@ -1,6 +1,11 @@
 'use strict'
 
-const pagination = ({ total, activePage }) =>
-  Array.from({ length: total }, (_, i) => i + 1)
+const pagination = ({ total, activePage }) => {
+  if (total <= 5) {
+    return Array.from({ length: total }, (_, i) => i + 1)
+  }
+
+  return [1, 2, 3, '...', 6]
+}
 
 export default pagination
--- examples/m02/applications/github-app/src/utils/pagination/pagination.test.js
+++ examples/m02/applications/github-app/src/utils/pagination/pagination.test.js
@@ -20,3 +20,17 @@ test('pagination({ total: 2, activePage: 1 }) should return [1, 2]', () => {
 
   expect(pagination(params)).to.be.deep.equal(result)
 })
+
+test('pagination({ total: 5, activePage: 1 }) should return [1, 2, 3, 4, 5]', () => {
+  const params = { total: 5, activePage: 1 }
+  const result = [1, 2, 3, 4, 5]
+
+  expect(pagination(params)).to.be.deep.equal(result)
+})
+
+test('pagination({ total: 6, activePage: 1 }) should return [1, 2, 3, "...", 6]', () => {
+  const params = { total: 6, activePage: 1 }
+  const result = [1, 2, 3, '...', 6]
+
+  expect(pagination(params)).to.be.deep.equal(result)
+})
