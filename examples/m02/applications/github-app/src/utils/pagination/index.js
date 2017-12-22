0ab3b32912187f8cdc5377d689934ab60e9c193d
Sergio Henrique  Pereira Reis <sergiohpreis@Sergios-MacBook-Pro.local>
2017-12-22 08:46:11 -0200

    M2#A54 - Paginação - implementação - parte 3

--- examples/m02/applications/github-app/src/utils/pagination/index.js
+++ examples/m02/applications/github-app/src/utils/pagination/index.js
@@ -1,11 +1,28 @@
 'use strict'
 
+const centerRule = ({total, activePage}) => {
+  if (activePage - 1 <= 0) {
+    return 1
+  }
+
+  return activePage - 1
+}
+
 const pagination = ({ total, activePage }) => {
   if (total <= 5) {
     return Array.from({ length: total }, (_, i) => i + 1)
   }
 
-  return [1, 2, 3, '...', 6]
+  const visiblePages = 3
+  let pages = [
+    1,
+    ...Array.from({ length: visiblePages }, (_, i) => i + centerRule({ total, activePage })),
+    total
+  ]
+
+  pages = pages.filter((page, index, array) => array.indexOf(page) === index)
+
+  return pages
 }
 
 export default pagination
--- examples/m02/applications/github-app/src/utils/pagination/pagination.test.js
+++ examples/m02/applications/github-app/src/utils/pagination/pagination.test.js
@@ -34,3 +34,17 @@ test('pagination({ total: 6, activePage: 1 }) should return [1, 2, 3, "...", 6]'
 
   expect(pagination(params)).to.be.deep.equal(result)
 })
+
+test('pagination({ total: 6, activePage: 2 }) should return [1, 2, 3, "...", 6]', () => {
+  const params = { total: 6, activePage: 2 }
+  const result = [1, 2, 3, '...', 6]
+
+  expect(pagination(params)).to.be.deep.equal(result)
+})
+
+test('pagination({ total: 6, activePage: 3 }) should return [1, 2, 3, 4, 5, 6]', () => {
+  const params = { total: 6, activePage: 3 }
+  const result = [1, 2, 3, 4, 5, 6]
+
+  expect(pagination(params)).to.be.deep.equal(result)
+})
