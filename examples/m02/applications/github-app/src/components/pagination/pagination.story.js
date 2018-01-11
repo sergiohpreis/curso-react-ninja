93fc9f0f6f9b0a94119e3a946a30f9cbd961fb2d
Sergio Henrique Pereira Reis <sergio.reis@hiplatform.com>
2018-01-11 19:20:48 -0200

    M2#A58 - Paginação - storybook

---
+++ examples/m02/applications/github-app/src/components/pagination/pagination.story.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import React from 'react'
+import { storiesOf } from '@kadira/storybook'
+
+import Pagination from './index'
+
+const stories = storiesOf('<Pagination />', module)
+
+stories.add('without props', () => (
+  <Pagination total={10}/>
+))
