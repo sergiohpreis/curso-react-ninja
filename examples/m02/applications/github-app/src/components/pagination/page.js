696d35baf0188ef203576dd66fa1844b0c94150d
Sergio Henrique  Pereira Reis <sergio.hpreis@gmail.com>
2018-01-23 23:53:52 -0200

    M2#A61 - Paginação - storybook - parte 4

--- examples/m02/applications/github-app/src/components/pagination/page.js
+++ examples/m02/applications/github-app/src/components/pagination/page.js
@@ -2,9 +2,10 @@
 
 import React from 'react'
 
+const Dots = () => <span>...</span>
+
 const Page = ({ page, pageLink, onClick }) => {
-  const Component = page === '...' ? 'span' : 'a'
-  const href = page === '...' ? null : pageLink
+  const Component = page === '...' ? Dots : 'a'
 
   const handleClick = !onClick ? null : (e) => {
     e.preventDefault()
@@ -12,7 +13,7 @@ const Page = ({ page, pageLink, onClick }) => {
   }
 
   return (
-    <Component href={href} onClick={handleClick} >
+    <Component href={pageLink} onClick={handleClick} >
       {page}
     </Component>
   )
