ecb187050e8805a4eace5d944e08bb16132d5bc1
Sergio Henrique  Pereira Reis <sergio.hpreis@gmail.com>
2018-01-24 00:04:20 -0200

    M2#A62 - Abstraindo configuração padrão do webpack

---
+++ examples/m02/applications/github-app/webpack/common.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const path = require('path')
+const webpack = require('webpack')
+
+const HtmlPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index'),
+
+  output: {
+    path: path.join(__dirname, '..', 'dist'),
+    filename: '[name]-[hash].js'
+  },
+
+  htmlPluginConfig: {
+    title: 'Github app',
+    template: path.join(__dirname, '..', 'src', 'html', 'template-dev.html')
+  },
+
+  standardPreLoader: {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    include: /src/,
+    loader: 'standard'
+  },
+
+  jsLoader: {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    include: /src/,
+    loader: 'babel'
+  },
+
+  cssLoader: {
+    test: /\.css$/,
+    exclude: /node_modules/,
+    include: /src/,
+    loaders: ['style', 'css']
+  },
+
+  resolve: {
+    alias: {
+      src: path.join(__dirname, '..', 'src'),
+      components: path.join(__dirname, '..', 'src', 'components')
+    }
+  }
+}
