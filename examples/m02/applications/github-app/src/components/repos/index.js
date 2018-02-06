3eda55f1cdb82ba6038364228f25bf4e3ff85d5b
Sergio Henrique  Pereira Reis <sergio.hpreis@gmail.com>
2018-02-06 20:16:29 -0200

    M2#A69 - Ajustando as proptypes do state para paginação

--- examples/m02/applications/github-app/src/components/repos/index.js
+++ examples/m02/applications/github-app/src/components/repos/index.js
@@ -30,7 +30,16 @@ Repos.propType = {
   className: PropTypes.string,
   title: PropTypes.string.isRequired,
   handlePagination: PropTypes.func.isRequired,
-  repos: PropTypes.object
+  repos: PropTypes.shape({
+    repos: PropTypes.arrayOf(PropTypes.shape({
+      link: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired
+    })).isRequired,
+    pagination: PropTypes.shape({
+      total: PropTypes.number,
+      activePage: PropTypes.number
+    }).isRequired
+  })
 }
 
 export default Repos
