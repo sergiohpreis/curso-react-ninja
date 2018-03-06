759e273ea644a4976d40783a96af025c3accb2ae
Sergio Henrique Pereira Reis <sergio.reis@hiplatform.com>
2018-03-06 17:51:57 -0300

    M2#A103 - Aplicação: editor de Markdown - ordenando os scripts no HTML | Parte 2

--- examples/m02/applications/markdown-editor/webpack/prod.config.js
+++ examples/m02/applications/markdown-editor/webpack/prod.config.js
@@ -14,8 +14,6 @@ module.exports = {
   output: common.output,
 
   plugins: [
-    new BundleAnalyzerPlugin(),
-
     new CleanPlugin(['dist'], {
       root: common.paths.root
     }),
@@ -47,15 +45,22 @@ module.exports = {
     }),
 
     new HtmlPlugin(Object.assign({}, common.htmlPluginConfig, {
-      chunkSortMode: (chunk1, chunk2) -> {
+      minify: { collapseWhitespace: true },
 
+      chunksSortMode: (chunk1, chunk2) => {
+        const order = ['react-build', 'vendor', 'main'];
+        const left = order.indexOf(chunk1.names[0]);
+        const right = order.indexOf(chunk2.names[0]);
+        return left - right;
       }
     })),
 
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true
     })
-  ],
+  ].concat(
+    process.env.ANALYZER ? new BundleAnalyzerPlugin() : []
+  ),
 
   module: {
     rules: [
