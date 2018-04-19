476b21dfc1ffc12d2ffbeb5c06f37fa427a76596
Sergio Henrique  Pereira Reis <sergio.hpreis@gmail.com>
2018-04-19 00:00:10 -0300

    M2#A113 - Aplicação: editor de Markdown - criar arquivos MD

--- examples/m02/applications/markdown-editor/src/components/markdown-editor/header.js
+++ examples/m02/applications/markdown-editor/src/components/markdown-editor/header.js
@@ -1,10 +1,14 @@
 import React, {PropTypes} from 'react'
 
-const MarkdownEditorHeader = ({ isSaving, handleRemove }) => (
+const MarkdownEditorHeader = ({ isSaving, handleRemove, handleCreate }) => (
   <header className='editor-header'>
     <p className='save-message'>
       {isSaving ? 'Salvando...' : 'Salvo!'}
     </p>
+
+    <button onClick={handleCreate}>
+      Criar novo
+    </button>
 
     <button onClick={handleRemove} className='button-remove'>
       Remover
@@ -14,7 +18,8 @@ const MarkdownEditorHeader = ({ isSaving, handleRemove }) => (
 
 MarkdownEditorHeader.PropTypes = {
   isSaving: PropTypes.bool.isRequired,
-  handleRemove: PropTypes.func.isRequired
+  handleRemove: PropTypes.func.isRequired,
+  handleCreate: PropTypes.func.isRequired
 }
 
 export default MarkdownEditorHeader
